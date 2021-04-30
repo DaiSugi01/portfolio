cac32254af41be24e199c5aba32ff3269e3f1c1a
Daisugi <volble124@gmail.com>
2021-04-29 22:28:26 -0700

    add

---
+++ pages/about-me.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout"
+
+const AboutMe = () => {
+  return (
+    <Layout title="About me">
+      Daiki Sugihara
+    </Layout>
+  )
+}
+
+export default AboutMe
