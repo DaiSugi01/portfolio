9b1fdb93e996fbc64572ec080de321380d740b52
Daisugi <volble124@gmail.com>
2021-05-04 14:00:54 -0700

    add

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,9 @@
 export default function Project() {
   return (
     <div className="h-screen w-full">
-      {/* <div className="h-main w-full">
-        <p className="text-4xl">Daiki Sugihara</p>
-      </div> */}
+      <div className="h-main w-full">
+        <p className="text-4xl text-center mt-16">Projects</p>
+      </div>
     </div>
   );
 }
