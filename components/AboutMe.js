b30466de63b4349682275dfb86657ac527a9566f
Daisugi <volble124@gmail.com>
2021-05-03 22:25:47 -0700

    add

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -4,13 +4,13 @@ import profile from "../data/profile.json";
 export default function AboutMe({ programming, others }) {
 
   return (
-    <div className="h-screen w-full bg-sub-main">
+    <div className="h-full w-full bg-sub-main pb-20">
       <div className="w-full block">
         <h1 className="font-bold text-4xl text-center mt-16">{profile.greet}</h1>
         <h2 className="mt-10 w-7/12 text-center m-auto text-xl">{profile.aboutMe}</h2>
       </div>
 
-      <div className="mt-28 flex w-11/12 m-auto">
+      <div className="mt-24 flex w-11/12 m-auto">
         {programming && programming.map(skill => <Card key={skill.id} content={skill}/>)}
       </div>
 
