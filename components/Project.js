4deb1048162f93effa6a6f6f6f4675006fa6e3dd
Daisugi <volble124@gmail.com>
2021-05-04 19:31:10 -0700

    add project section

--- components/Project.js
+++ components/Project.js
@@ -1,8 +1,30 @@
+import Filter from "../components/Filter";
+import { useState } from "react";
+
 export default function Project() {
+  const [activeIndex, setActive] = useState(0);
+  const filters = ['All', 'Web', 'iOS']
+
+  const onChangeActive = active => {
+    setActive(active);
+  };
+  
   return (
     <div className="h-screen w-full">
       <div className="h-main w-full">
-        <p className="text-4xl text-center mt-16">Projects</p>
+        <p className="text-5xl text-center mt-16">Projects</p>
+        <div className="mx-auto mt-4 mb-16 border-solid w-16 border-white border-2"></div>
+
+        <div className="flex justify-center text-gray-300 items-center w-full">
+          {filters.map((buttonName, i) => (
+            <Filter
+              key={i}
+              text={buttonName}
+              onActive={() => onChangeActive(i)}
+              active={i === activeIndex}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
