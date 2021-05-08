de1589637dd157f2eb84f23b561710b1daf3c070
Daisugi <volble124@gmail.com>
2021-05-08 04:15:47 -0700

    add logo

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,17 +1,31 @@
+import styled from "styled-components";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
-
 import { SnsIcon } from "../components/Common";
 
 const styles = {
   fontSize: 30
 };
 
+const Logo = styled.div`
+  height: 6rem;
+  background-color: #C58753;
+  font-family: Great Vibes;
+  text-align: center;
+  align-items: center;
+  font-size: 1.8rem;
+  line-height: 6rem;
+  font-weight: 700;
+`;
+
 export default function Header() {
 
   return (
-    <nav className="w-20 min-h-screen bg-gray-800 fixed top-0 z-20">
-      <div className="flex flex-col items-center min-h-screen">
+    <nav className="w-20 min-h-screen bg-gray-800 fixed top-0 z-20 flex flex-col justify-between">
+      <Logo>
+        DS
+      </Logo>
+      <div className="flex flex-col items-center">
         <SnsIcon
           href="https://github.com/DaiSugi01"
           target="_blank"
