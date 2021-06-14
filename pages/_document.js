5380830d1797248fa7a389a2c47f987b67488db1
Daisugi <volble124@gmail.com>
2021-06-13 22:05:34 -0700

    modified

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,8 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link
-            rel="preload"
+            rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-            as="style"
           />
         </Head>
         <body>
