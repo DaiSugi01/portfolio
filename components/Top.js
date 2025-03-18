6627b24cd8c7f8c764c27a42af1e99f937016ee1
Daiki Sugihara <daikisugihara@DaikinoMacBook-Pro.local>
2025-03-18 23:18:10 +0900

    fix

--- components/Top.js
+++ components/Top.js
@@ -41,7 +41,7 @@ export default function Top() {
           Daiki Sugihara
         </Box>
         <Box component="p" display="block" className={classes.jobTitle}>
-          Web designer / Backend developer
+          Web designer / Software developer
         </Box>
       </Box>
 
